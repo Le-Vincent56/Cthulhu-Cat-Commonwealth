4014d136ca2cab01878f7086f2869f79b8a8279a
Ryan Zhang <zhangryan09@gmail.com>
2024-11-16 16:43:15 -0500

    Created intro cutscene

---
+++ Tethered/Assets/Scripts/Cutscene.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Cutscene : MonoBehaviour
+{
+    [SerializeField] private float appearSpeed;
+    [SerializeField] private float scrollSpeed;
+
+    [SerializeField] private TMP_Text[] texts;
+    [SerializeField] private Image bg;
+    [SerializeField] private SpriteRenderer fade;
+
+    [SerializeField] private Vector3 finalPos;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (var text in texts)
+        {
+            text.alpha = 0;
+        }
+
+        DOTween.Sequence()
+            // Part 1
+            .AppendInterval(scrollSpeed)
+            .Append(
+                DOTween.To(() => texts[0].alpha, x => texts[0].alpha = x, 1, appearSpeed)
+            )
+            .AppendInterval(scrollSpeed)
+            .Append(
+                DOTween.To(() => texts[1].alpha, x => texts[1].alpha = x, 1, appearSpeed)
+            )
+            .AppendInterval(scrollSpeed + 0.4f)
+            .AppendCallback(() =>
+            {
+                DOTween.To(() => texts[0].alpha, x => texts[0].alpha = x, 0, appearSpeed);
+                DOTween.To(() => texts[1].alpha, x => texts[1].alpha = x, 0, appearSpeed);
+            })
+            // Part 2
+            .AppendInterval(scrollSpeed)
+            .Append(
+                DOTween.To(() => texts[2].alpha, x => texts[2].alpha = x, 1, appearSpeed)
+            )
+            .AppendInterval(scrollSpeed)
+            .Append(
+                DOTween.To(() => texts[3].alpha, x => texts[3].alpha = x, 1, appearSpeed)
+            )
+            .AppendInterval(scrollSpeed + 0.4f)
+            .AppendCallback(() =>
+            {
+                DOTween.To(() => texts[2].alpha, x => texts[2].alpha = x, 0, appearSpeed);
+                DOTween.To(() => texts[3].alpha, x => texts[3].alpha = x, 0, appearSpeed);
+            })
+            .Append(
+                DOTween.To(() => texts[4].alpha, x => texts[4].alpha = x, 1, 0.01f)
+            )
+            .Append(
+                texts[4].rectTransform.DOShakeAnchorPos(1f, 40, 32, 20, 
+                    false, true, ShakeRandomnessMode.Harmonic)
+            )
+            .Append(
+                DOTween.To(() => texts[4].alpha, x => texts[4].alpha = x, 0, 0.1f)
+            )
+            .Append(
+                DOTween.To(() => bg.color, x => bg.color = x, Color.clear, 0.8f)
+            )
+            .AppendInterval(1f)
+            .Append(
+                DOTween.To(() => Camera.main.transform.position, p => Camera.main.transform.position = p, finalPos, 10f)
+            )
+            .Append(
+                DOTween.To(() => fade.color, x => fade.color = x, Color.black, 1.5f)
+            )
+            .Append(
+                DOTween.To(() => bg.color, x => bg.color = x, Color.black, 0.2f)
+            )
+            .AppendCallback(() =>
+                {
+                    SceneManager.LoadScene("Scenes/MockLevel1");
+                }
+            );
+    }
+}
